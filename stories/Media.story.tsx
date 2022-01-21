425950c45e60f6331c7e65a4cdaec313a37b1236
Federico Milone <federico.milone@claro.com.ar>
2022-01-21 11:53:05 -0300

    update;

--- stories/Media.story.tsx
+++ stories/Media.story.tsx
@@ -3,6 +3,7 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Media } from "../src";
 import { MediaInterface } from "../types";
 import { ICONS } from "../src/constants/icons";
+import { COLORS } from "../src/constants";
 
 export default {
     title: "Media",
@@ -12,7 +13,8 @@ export default {
             default: "facebook",
             values: [
                 { name: "twitter", value: "#00aced" },
-                { name: "facebook", value: "#3b5998" }
+                { name: "facebook", value: "#3b5998" },
+                { name: "tealBlue", value: COLORS.tealBlue }
             ]
         }
     }
@@ -27,5 +29,88 @@ Icons.args = {
 };
 
 Icons.parameters = {
-    backgrounds: { default: "facebook" }
+    backgrounds: { default: "tealBlue" }
 };
+
+Icons.decorators = [
+    () => {
+        return (
+            <>
+                <div>
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.CHECK}
+                        alt={ICONS.CHECK}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.CHECK_WHITE}
+                        alt={ICONS.CHECK_WHITE}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.ERROR_LAUNCH}
+                        alt={ICONS.ERROR_LAUNCH}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.FLAG_ARG}
+                        alt={ICONS.FLAG_ARG}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.INFO_BLUE}
+                        alt={ICONS.INFO_BLUE}
+                        margin={"1rem"}
+                    />
+                </div>
+                <div>
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.LOGO}
+                        alt={ICONS.LOGO}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.LOGOUT}
+                        alt={ICONS.LOGOUT}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.INFO_WHITE}
+                        alt={ICONS.INFO_WHITE}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.INFO_WHITE}
+                        alt={ICONS.INFO_WHITE}
+                        margin={"1rem"}
+                    />
+                    <Media
+                        {...(Icons.args as MediaInterface)}
+                        src={ICONS.INFO_WHITE}
+                        alt={ICONS.INFO_WHITE}
+                        margin={"1rem"}
+                    />
+                </div>
+            </>
+        );
+    }
+];
+
+
+
+
+//     LOGOUT: logout,
+//     SEARCH: search,
+//     SEARCH_SIDE_BAR: searchSideBar,
+//     VERIFY: verify,
+//     VERTICAL_OFF: verticalOff,
+//     VERTICAL_OK: verticalOk
